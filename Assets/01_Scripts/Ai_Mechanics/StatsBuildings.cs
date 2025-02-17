e554011b61e4956c9e24c6d18eec4d6ab181ebf4
AniDrag <nikecoblak@gmail.com>
2025-02-17 15:50:39 +0100

    Main_5

--- Assets/01_Scripts/Ai_Mechanics/StatsBuildings.cs
+++ Assets/01_Scripts/Ai_Mechanics/StatsBuildings.cs
@@ -1,31 +1,73 @@
+using System.Collections;
 using UnityEngine;
 [RequireComponent(typeof(DealAnnoyance))]
 public class StatsBuildings : MonoBehaviour
 {
-    [Header("Building settings")]
+    [Header("--- Building stats ---")]
+    public int maxAnnoyance = 100;
+    public int currentAnnoyance = 0;
+    [Header("--- Building settings ---")]
     [SerializeField] AI_Collections AiSpawnList;
-    [SerializeField] float timeBetweenNPCspawns;
-    [SerializeField] int residentCount;
-    [SerializeField] int maxWaves;
+    [SerializeField] float timeBetweenNPCspawns = 3;
+    [SerializeField] int residentCount = 8;
+    [SerializeField] int maxWaves = 4;
+
+    //gets annoyed and spawns a npc
+    // private stuff
     [SerializeField] Transform aiSpawnocation;
-
-
+    // calcualte an int for spawns with a while
     int currentWave;
-    int spawnedNPCs;
-    float timer;
+    int waveTreshold;
+    int oldWaveTreshold;
+    int addToWaveTreshold;
     void Start()
     {
-        
+        aiSpawnocation = transform.GetChild(3).transform;
+        addToWaveTreshold = maxAnnoyance / maxWaves;
+        currentWave = 0;
+        waveTreshold = addToWaveTreshold;
     }
 
-    void Update()
-    {
-        
-    }
+   
 
     public void AnnoyTarget( int amount)
     {
         Debug.Log("Target annoyed for: " + amount);
         Game_Manager.instance.GetPoints(amount);
+        // checs if annoyance is maxed
+        if (currentAnnoyance > maxAnnoyance) currentAnnoyance = maxAnnoyance;
+        CheckAnnoyanceLevel();
+    }
+    void CheckAnnoyanceLevel()
+    {
+        if(currentAnnoyance >= waveTreshold)
+        {
+            currentWave++;
+            oldWaveTreshold = waveTreshold;
+            waveTreshold += addToWaveTreshold;
+            StartCoroutine(SpwanAI());
+        }
+    }
+
+    IEnumerator SpwanAI()
+    {
+        int spawnCount = residentCount / maxWaves;
+        int counter = 0;
+
+        while (counter <= spawnCount)
+        {
+            counter++;
+            int index = Random.Range(0, AiSpawnList.allResidentVariants.Length);
+            Instantiate(AiSpawnList.allResidentVariants[index], aiSpawnocation);
+            yield return new WaitForSeconds(timeBetweenNPCspawns);
+        }
+        CheckAnnoyanceLevel();
+        
+    }
+    void OnDrawGizmos()
+    {
+        // Player Height & Width (Purple)
+        Gizmos.color = new Color(0.5f, 0.5f, 0.5f); // Purple with transparency
+        Gizmos.DrawSphere(aiSpawnocation.position, 0.3f);
     }
 }
